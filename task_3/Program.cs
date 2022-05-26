61b920e3bd128df574e21bbafc6aae4ec217bb04
Aleksandra Popova <alexnika@bk.ru>
2022-05-26 14:36:56 +0300

    added changes in  task_3

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -85,7 +85,7 @@ else
 _________________________________________________________________*/
 
 // Второй вариант
-
+/*
 void PrintMatrix(int[,] matrix)
 {
     for(int i=0;i<matrix.GetLength(0);i++)
@@ -130,4 +130,66 @@ int value = int.Parse(Console.ReadLine() ?? "0");
 Console.WriteLine(position != null 
     ? $"Position of element: {position.Value.Item1} {position.Value.Item2}" // да
     : "There is no value in matrix");// нет
+    ________________________________________________________________________*/
 
+void FillArray(int[,] array)
+{
+    Random rnd           = new Random();
+    int    rowsLength    = array.GetLength(0);
+    int    columnsLength = array.GetLength(1);
+    for (int i = 0; i < rowsLength; i++)
+    {
+        for (int j = 0; j < columnsLength; j++)
+        {
+            array[i,j] = rnd.Next(0,2);
+        }       
+    }
+}
+//--------------------------------------------------------------------------------------------+
+void SearchElement(int[,] array, int number)
+{
+    int    rowsLength    = array.GetLength(0);
+    int    columnsLength = array.GetLength(1);
+    bool   found         = false;
+    for (int i = 0; i < rowsLength; i++)
+    {
+        for (int j = 0; j < columnsLength; j++)
+        {
+            if(array[i,j] == number)
+            {
+             Console.WriteLine($"Элемент {number} находится на позиции [{i},{j}]");
+             found = true;
+            }
+        }       
+    }
+    if(!found) Console.WriteLine($"Элемент {number} не найден в массиве!");
+}
+//--------------------------------------------------------------------------------------------+
+void PrintArray(int[,] array)
+{
+    int    rowsLength    = array.GetLength(0);
+    int    columnsLength = array.GetLength(1);
+    for (int i = 0; i < rowsLength; i++)
+    {
+        for (int j = 0; j < columnsLength; j++)
+        {
+            Console.Write(array[i,j] + " ");
+        } 
+        Console.WriteLine();      
+    }
+
+}
+//--------------------------------------------------------------------------------------------+
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int rows = int.Parse(Console.ReadLine()?? "");
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine()?? "");
+Console.Write("Введите число для поиска в массиве: ");
+int number = int.Parse(Console.ReadLine()?? "");
+
+int[,] array = new int[rows,columns];
+
+FillArray(array);
+PrintArray(array);
+SearchElement(array,number);
